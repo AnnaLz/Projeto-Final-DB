02a6746b1e53be0face0cf580bf5fff63e49aaa7
Anna Luiza <anna.luiza@unifei.edu.br>
2021-12-15 19:47:07 -0300

    commit

---
+++ server.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const  express = require("express");
+const  bodyParser = require("body-parser");
+
+const  app = express();
+
+const  port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+res.send("Hello World");
+});
+
+app.listen(port, () => {
+
+console.log(`Server running at http://localhost:${port}`);});
+
+require("./config/db");
+
+var routeUser = require('./api/routes/usuariosRoutes');
+var routeVinhos = require('./api/routes/vinhosRoutes');
+var routeUvas = require('./api/routes/uvasRoutes');
+var routeTipoVinhos = require('./api/routes/tipoVinhosRoutes');
+var routeHarmonizacoes = require('./api/routes/harmonizacoesRoutes');
+
+routeUser(app);
+routeVinhos(app);
+routeUvas(app);
+routeTipoVinhos(app);
+routeHarmonizacoes(app);
